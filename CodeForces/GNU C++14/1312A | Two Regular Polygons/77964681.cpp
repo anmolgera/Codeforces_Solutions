68d2e9c0e19d9aaf28792db34f5501682f2e1494
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 01:43:08 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1312A | Two Regular Polygons/77964681.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main(){
+ 
+ 
+#ifndef ONLINE_JUDGE
+ freopen("input.txt","r",stdin);
+ freopen("output.txt","w",stdout);
+#endif
+ 
+ 
+int n;
+cin>>n;
+while(n--){
+ 
+int a,b;
+cin>>a>>b;
+ 
+if(a%b==0){
+ cout<<"YES"<<endl;
+}
+ 
+else cout<<"NO"<<endl;
+ 
+ 
+ 
+}}
