92210fcfa08fb6f3842dc7d81610f321d4710a07
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 00:41:51 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1525D | Armchairs/116392452.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define int long long
+//typedef pair<int, int> pii;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+const ll mod = 1e9 + 7, N = 2e6 + 7, M = 2e6 + 7, INF = INT_MAX / 10;
+ll powe(ll x, ll y)
+{
+    x = x % mod, y = y % (mod - 1);
+    ll ans = 1;
+    while (y > 0)
+    {
+        if (y & 1)
+        {
+            ans = (1ll * x * ans) % mod;
+        }
+        y >>= 1;
+        x = (1ll * x * x) % mod;
+    }
+    return ans;
+}
+ 
+void anmol()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int32_t main()
+{
+     anmol();
+     int t;
+     //cin>>t;
+     t =1;
+ 
+     while(t--){
+      
+       int n;
+       cin>>n;
+       int a[n];
+       vector<int> from ={0};
+       vector<int> to ={0};
+       for(int i =0; i<n; i++){
+           cin>>a[i];
+           if(a[i]){
+               from.push_back(i);
+           }
+           else {
+               to.push_back(i);
+           }
+       }
+      int x = from.size();
+      int y = to.size();
+      vector<vector<int>> dp(x,vector<int>(y,INF));
+      for(int i =0; i<y; i++){
+          dp[0][i] =0;
+      }
+      for(int i =1; i<=x-1; i++){
+          for(int j =1; j<=y-1; j++){
+                dp[i][j] = min(dp[i][j-1], abs(from[i]-to[j])+dp[i-1][j-1]);
+          }
+      }
+ 
+      cout<<dp[x-1][y-1]<<endl;
+      //dp[i][j] = min(dp[i][j+1],shifting the element from i to j);
+      
+ 
+ 
+ 
+     }
+     
+     
+ }
+ 
+ // 5 2 4 3 1  // 2 4 1 5 3 // 1 2 4 5 3 // 
+ // 5 1 2 3 4
+ // 1 2 3 5 4
+ // 1 2 3 4 5
