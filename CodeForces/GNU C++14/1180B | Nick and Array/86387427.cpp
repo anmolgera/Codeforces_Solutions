925b178e95c8e0bd77b9b12c12592daa8d794d82
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 01:25:32 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1180B | Nick and Array/86387427.cpp
@@ -0,0 +1,364 @@
+ 
+        
+        
+      #include<bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+ 
+ 
+#define int             long long
+ 
+ 
+void anmol()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+const int N = 1e6 + 5 ;
+ int a[N], b[N] , freq[N];
+ 
+ 
+   int sum(int n){
+ 
+    int count =0;
+     while(n!=0){
+   
+     count+=n%10;
+     n = n/10;
+ 
+ 
+ 
+    }
+ 
+    return count;
+ 
+   }
+ 
+ 
+ 
+int32_t main(){
+ 
+  anmol();
+ 
+  int n;
+  cin>>n;
+ 
+  for(int i =0; i<n; i++){
+ 
+    cin>>a[i];
+ 
+ 
+ 
+  }
+ 
+  
+  for(int i =0; i<n; i++){
+ 
+   if(a[i]>=0){
+ 
+ 
+    a[i] = (-1)*a[i];
+    a[i] = a[i]-1;
+ 
+   }
+ 
+ 
+ 
+  }
+ 
+   if(n%2==0){
+ 
+ 
+    for(int i =0; i<n; i++){
+ 
+     cout<<a[i]<<" ";
+ 
+ 
+    }
+  
+  cout<<endl;
+ 
+   }
+ 
+ 
+   else {
+     
+ 
+        int index =-1;
+ 
+        int min = INT_MAX;
+ 
+ 
+ 
+        for(int i =0; i<n; i++){
+ 
+             
+             if(a[i]<min && a[i]!=-1){
+ 
+              min = a[i];
+              index =i;
+             }
+         
+ 
+        }
+ 
+    
+      if(index ==-1){
+ 
+        a[0] =0;
+        cout<<a[0]<<" ";
+ 
+        for(int i=1; i<n; i++){
+ 
+          cout<<a[i]<<" ";
+        }
+        
+    cout<<endl;
+ 
+ 
+      }
+ 
+    else{ 
+ 
+      a[index] = (-1)*a[index];
+     a[index] = a[index]-1;
+ 
+ 
+     for(int i =0; i<n; i++){
+ 
+       cout<<a[i]<<" ";
+     }
+ 
+     cout<<endl;
+ 
+    }
+ 
+ 
+ 
+   }
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+ 
+  }
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+   
+ 
+  
+  
+ 
+   
+   
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+    
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+    
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+    
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+    
+ 
+  
