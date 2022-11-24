8248e3a1eae41bcee661de6dbd7d35f2a8179fb9
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 01:35:01 +0530

    Committing files

---
+++ CodeForces/GNU C++14/519C | A and B and Team Training/81908448.cpp
@@ -0,0 +1,198 @@
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+ 
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+//#define vi              vector<int>
+#define mii             map<int,int>
+#define mo              unordered_map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define ml(brr,n,type)  type *brr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+ 
+ 
+void anmol()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+const int N = 1e6 + 5 ;
+int a[N] , dp[N] , brr[N];
+ 
+ 
+int get(int n){
+int i =2;
+ for(; i*i<=n; i++){
+ 
+  if(n%i==0){
+   return i;
+ 
+  }
+ }
+ 
+return n;
+ 
+}
+ 
+ 
+int32_t main()
+{
+   anmol();
+ 
+   int a, b;
+   cin>>a>>b;
+  
+ 
+  if(a+b<=2){
+   cout<<0<<endl;
+  }
+ 
+ 
+  else if(a==1 && a+b>=3 || b==1 && a+b >=3){
+ 
+   cout<<1<<endl;
+  }
+ 
+ 
+  else if (max(a,b)>=2*min(a,b)){
+ 
+   cout<<min(a,b)<<endl;
+  }
+ 
+ 
+  else {
+ 
+ 
+   cout<<(a+b)/3<<endl;
+ 
+ 
+ 
+ 
+ 
+  }
+}
+ 
+  
+ 
+ 
+ 
+ 
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+    
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+    
+ 
+  
