bbcb4b507b0f94ceb18f7edd527e94e9539f264a
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 00:56:08 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1467A | Wizard of Orz/103757692.cpp
@@ -0,0 +1,99 @@
+/* Author : Anmol Gera
+NSIT 4th Year */
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+ 
+ 
+void anmol()
+{
+  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+bool isPerfectSquare(long double x) 
+{   
+  // Find floating point value of  
+  // square root of x. 
+  long double sr = sqrt(x); 
+  
+  // If square root is an integer 
+  return ((sr - floor(sr)) == 0); 
+} 
+ 
+/*
+while(lo < hi) {
+int mid = lo + (hi - lo) / 2;
+if(Special condition passed)(optional):
+  return mid; 
+if(condition passed)
+  hi = mid;
+else 
+  lo = mid + 1;
+}
+return lo;
+*/
+ 
+void solve(){
+ 
+int n;
+cin>>n;
+string s =  "989";
+string s1 = "0123456789";
+string res  ="";
+int flag =0;
+int x = s.length();
+int i =0;
+for(i =0; i<n; i++){
+  
+    if(i<x)res+=s[i];
+    else {
+      break;
+    }
+ 
+  
+}
+//cout<<i<<endl;
+n-=i;
+for(int j = 0; j<n; j++){
+  res+=s1[j%10];
+}
+cout<<res<<endl;
+}
+ 
+int32_t main(){
+ 
+ 
+  anmol();
+  int t;
+  ///t =1;
+  cin>>t;
+  while(t--){
+ 
+ 
+        //hhgfhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+        //hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+        //hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+        //hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+    ///  hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'b'
+ 
+        
+        //int a,b;
+        //cin>>a>>b;
+ 
+    solve();
+   //""""""""""""
+        //cout<<fact[a] - fact[b];
+        // look for base cases always 
+       
+//lkjgfdsaqwertyuiop.,mnbvcxz=-87564321`()hH" "
+ 
+ 
+ 
+  }
+ 
+}
