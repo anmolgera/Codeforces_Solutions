59abdf6b1268627def6d51729b84f6aec166c7d5
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 01:43:27 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1337A | Ichihime and Triangle/76862016.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ 
+int n;
+cin>>n;
+while(n--){
+int a[4];
+for( int i =1; i<=4;i++){
+    cin>>a[i];
+}
+ 
+ 
+//int x =0,y =0,z=0;
+/*int flag =0;
+int i,j,k;
+for ( i=a[1],j=a[2],k=a[3]; i<=a[2]&&j<=a[3]&&k<=[4]; i++,j++,k++){
+ 
+ 
+            if( i+j>k /*&& j+k>=i && k+i>=j){
+                cout<<i<<" "<<j<<" "<<k<<endl;
+                flag =1;
+                break;
+            }
+            else {
+                continue;
+            }
+ 
+ 
+ 
+}
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+*/
+ 
+cout<<a[1]<<" "<<a[3]<<" "<<a[3]<<endl;
+ 
+ 
+}
+ 
+}
+ 
+ 
+ 
