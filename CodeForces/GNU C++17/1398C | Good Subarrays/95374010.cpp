0d866c5a42383617082030f0b3bd8eda23e778e0
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 01:08:45 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1398C | Good Subarrays/95374010.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+//vector<int> adj[200005];
+//vector<int> vis(200005,0);
+//vector<int> deg(200005,0);
+void anmol()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+ 
+ 
+ 
+ int32_t main(){
+ 
+   anmol();
+    int t;
+    cin>>t;
+    while(t--){
+ 
+      int n;
+      cin>>n;
+ 
+      string s;
+      cin>>s;
+ 
+      int a[n];
+      map<int,int> mp;
+      for(int i =0; i<s.length(); i++){
+ 
+ 
+        a[i] = s[i]-'1';
+      }
+      
+      mp[0]=1;
+      int sum =0;
+      int cnt =0;
+      for(int i =0; i<n; i++){
+        
+        sum+=a[i];
+        //cout<<sum<<" "<<"s"<<endl;
+        cnt+=mp[sum];
+       // cout<<cnt<<endl;
+        mp[sum]++;
+       
+       
+ 
+ 
+ 
+      }
+ 
+      cout<<cnt<<endl;
+ 
+ 
+ 
+    }
+  
+    }
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
