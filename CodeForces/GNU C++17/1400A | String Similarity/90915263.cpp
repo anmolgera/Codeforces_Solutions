5fb2a071b3b5102cd9b8704d668b378916dd17c3
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 01:19:36 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1400A | String Similarity/90915263.cpp
@@ -0,0 +1,64 @@
+ 
+ #include<bits/stdc++.h>
+#include<map>
+ 
+using namespace std;
+ 
+#define int long long
+ 
+ 
+void anmol()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("inpu1t.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+ 
+ 
+ 
+int32_t main() 
+{ 
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+ //anmol();
+ int t;
+ cin>>t;
+ //int j =1;
+ while(t--){
+ 
+ 
+int n;
+cin>>n;
+ 
+ 
+string s;
+cin>>s;
+ 
+string res ="";
+ 
+for(int i =0; i<s.length(); i=i+2){
+ 
+res+=s[i];
+}
+ 
+ 
+cout<<res<<endl;
+ 
+ 
+ 
+}}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    
+    
