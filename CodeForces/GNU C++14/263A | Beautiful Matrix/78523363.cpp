0619fbeee043547bc90a7dbf1b910cf9f067c211
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 01:41:46 +0530

    Committing files

---
+++ CodeForces/GNU C++14/263A | Beautiful Matrix/78523363.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+#include<string.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+ 
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define my              unordered_map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define ml(brr,n,type)  type *brr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+ 
+ 
+void anmol()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+ 
+bool sign(int num){
+ return num>0;
+}
+ 
+int32_t main()
+{
+   anmol();
+ 
+int n,x,y;
+ 
+ 
+ 
+for(int i =1; i<=5; i++){
+ for(int j =1; j<=5; j++){
+  cin>>n;
+ 
+  if(n==1){
+   cout<<(abs(3-i) +abs(3-j) )<<endl;
+  }
+ }
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    
+ 
+  
