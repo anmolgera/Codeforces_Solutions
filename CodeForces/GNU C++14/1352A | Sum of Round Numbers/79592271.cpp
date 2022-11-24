904abb0ff0569b3514321330bda4b4edbd610145
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 01:39:45 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1352A | Sum of Round Numbers/79592271.cpp
@@ -0,0 +1,118 @@
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+ 
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define my              unordered_map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define ml(brr,n,type)  type *brr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+ 
+ 
+void anmol()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+ 
+bool arraySortedOrNot(int arr[], int n) 
+{ 
+    // Array has one or no element 
+    if (n == 0 || n == 1) 
+        return true; 
+  
+    for (int i = 1; i < n; i++) 
+  
+        // Unsorted pair found 
+        if (arr[i - 1] > arr[i]) 
+            return false; 
+  
+    // No unsorted pair found 
+    return true; 
+} 
+ 
+bool sign(int num){
+ return num>0;
+}
+ 
+int32_t main()
+{
+   anmol();
+  
+  w(t){
+int n;
+cin>>n;
+vi v(0);
+ 
+ 
+ 
+int count =0;
+int i =0;
+while(n!=0){
+ 
+ 
+  if(n%10!=0){
+    
+    int x = n%10;
+    //cout<<x<<" ";
+    int y =x*pow(10,i);
+ 
+    if(y%10!=0 && i>=1){
+     y++;
+    }
+    //cout<<y<<" ";
+   v.push_back(y);
+   count++;
+   n = n/10;
+   i++;
+    
+  }
+  
+else {
+ n = n/10;
+ i++;
+}
+ 
+ 
+}
+ 
+cout<<count<<endl;
+for(int i =0; i<count; i++){
+ cout<<v[i]<<" ";
+}
+ 
+cout<<endl;
+}
+}
+ 
+  
+ 
+ 
+ 
+ 
+ 
+    
+ 
+  
