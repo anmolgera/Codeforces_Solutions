f0c7edec8b50552e56793dc9e9828d8a4a3d8307
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 01:43:34 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1328A | Divisibility Problem/74416634.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ 
+int n;
+cin>>n;
+while(n--){
+ 
+ 
+   int a,b;
+    cin>>a>>b;
+    int k;
+ 
+if(a%b==0){
+    cout<<0<<endl;
+}
+ 
+ 
+else cout<<b-a%b<<endl;
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
