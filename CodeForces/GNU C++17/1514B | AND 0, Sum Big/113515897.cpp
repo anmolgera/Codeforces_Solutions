c2f29365c6b292f4950a1160b9b80cc2ab5cccb7
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 00:44:38 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1514B | AND 0, Sum Big/113515897.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define int long long
+//typedef pair<int, int> pii;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+const ll mod = 1e9 + 7, N = 2e6 + 7, M = 2e6 + 7, INF = INT_MAX / 10;
+ll powe(ll x, ll y)
+{
+    x = x % mod, y = y % (mod - 1);
+    ll ans = 1;
+    while (y > 0)
+    {
+        if (y & 1)
+        {
+            ans = (1ll * x * ans) % mod;
+        }
+        y >>= 1;
+        x = (1ll * x * x) % mod;
+    }
+    return ans;
+}
+ 
+void anmol()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+ 
+ 
+ 
+ 
+int32_t main()
+{
+     anmol();
+     int t;
+     cin>>t;
+     int z =1;
+     //SieveOfEratosthenes();
+ 
+     while(t--){
+       //cout<<"Case #"<<z<<": ";
+       //z++;
+ 
+       int n,k;
+       cin>>n>>k;
+       int x = powe(n,k);
+       cout<<x<<endl;
+     }
+}
