e9aa6b90a511e3d4f319671d538738360b3226e5
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 01:33:21 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1326A | Bad Ugly Numbers/84143176.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+void anmol()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+ 
+const int N = 1e6 + 5 ;
+int a[N] , dp[N] , b[N] ;
+ 
+int main(){
+anmol();
+ 
+int t;
+cin>>t;
+ 
+while(t--){
+ 
+int n;
+cin>>n;
+ 
+if(n==1){
+ cout<<-1<<endl;
+}
+ 
+else {
+ 
+ cout<<2;
+ for(int i =0; i<n-1; i++){
+  cout<<3;
+ }
+ 
+ cout<<endl;
+}
+ 
+}
+ 
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
