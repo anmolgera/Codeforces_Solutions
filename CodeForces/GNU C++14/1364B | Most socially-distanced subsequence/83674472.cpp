7110210424c7e76ff4e59dd23a5895aef2ee5ff5
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 01:33:53 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1364B | Most socially-distanced subsequence/83674472.cpp
@@ -0,0 +1,168 @@
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+ 
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+//#define vi              vector<int>
+#define mii             map<int,int>
+#define my              unordered_map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define ml(brr,n,type)  type *brr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+ 
+ 
+void anmol()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+const int N = 1e6 + 5 ;
+int a[N] , dp[N] , b[N];
+ 
+ 
+ 
+ 
+ 
+int32_t main()
+{
+   anmol();
+  int t;
+  cin>>t;
+  
+  while(t--){
+ 
+  int n;
+  cin>>n;
+ 
+ 
+  for(int i =0; i<n; i++){
+ 
+   cin>>a[i];
+  }
+  
+  int len =0;
+ 
+  set<int> s;
+  vector<int > v;
+  v.push_back(0);
+  for(int i =1; i<n-1; i++){
+   
+  
+    if(a[i] > a[i-1] && a[i] > a[i+1]){
+ 
+ 
+ v.push_back(i);
+     
+}
+ 
+ if(a[i] < a[i-1] && a[i] < a[i+1]){
+ 
+ 
+ v.push_back(i);
+     
+}
+ 
+ 
+ 
+  
+  }
+  v.push_back(n-1);
+  
+ // cout<<v.size()<<endl;
+ 
+ 
+ 
+ /* for(int i =0; i<v.size(); i++){
+   cout<<v[i]<<" ";
+  }
+  cout<<endl;*/
+ 
+  vector<int> v1;
+  /*for(int i =0; i<v.size(); i++){
+ 
+   cout<<a[v[i]]<<" ";
+  }
+ 
+  cout<<endl;
+ 
+*/
+ 
+ 
+  v1.push_back(a[v[0]]);
+  for(int i =1; i<v.size()-1; i++){
+ 
+ 
+    if(a[v[i]]>a[v[i-1]] && a[v[i]] > a[v[i+1]]){
+     v1.push_back(a[v[i]]);
+    }
+ 
+    if(a[v[i]] < a[v[i-1]] && a[v[i]] < a[v[i+1]]){
+     v1.push_back(a[v[i]]);
+    }
+ 
+ 
+  }
+ 
+  v1.push_back(a[v[v.size()-1]]);
+ 
+ cout<<v1.size()<<endl;
+  for(int i =0; i<v1.size(); i++){
+ 
+   cout<<v1[i]<<" ";
+  }
+cout<<endl;
+    }
+ 
+ 
+}
+ 
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+    
+ 
+  
