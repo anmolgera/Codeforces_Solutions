5136d02d8a009b1345c6529999f839deb06a4963
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 01:02:44 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1445B | Elimination/97321316.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main(){
+ 
+int n;
+cin>>n;
+while(n--){
+ 
+ int a,b,c,d;
+ cin>>a>>b>>c>>d;
+ 
+cout<<max(a+b,c+d)<<endl;
+ 
+}
+}
+ 
+ 
+ 
