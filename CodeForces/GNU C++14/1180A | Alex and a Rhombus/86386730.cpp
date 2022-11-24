b8db3a5d98e2266246c92926b63cb42a2dfdd0b1
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 01:25:46 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1180A | Alex and a Rhombus/86386730.cpp
@@ -0,0 +1,271 @@
+ 
+        
+        
+      #include<bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+ 
+ 
+#define int             long long
+ 
+ 
+void anmol()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+const int N = 1e6 + 5 ;
+ int a[N], b[N] , freq[N];
+ 
+ 
+   int sum(int n){
+ 
+    int count =0;
+     while(n!=0){
+   
+     count+=n%10;
+     n = n/10;
+ 
+ 
+ 
+    }
+ 
+    return count;
+ 
+   }
+ 
+ 
+ 
+int32_t main(){
+ 
+  anmol();
+ 
+  int n;
+  cin>>n;
+   
+  int dp[101];
+ 
+  dp[1] =1;
+ 
+  for(int i =2; i<=n; i++){
+ 
+    dp[i] = dp[i-1] + 4*(i-1);
+ 
+ 
+  }
+  
+ 
+  cout<<dp[n];
+ 
+  }
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+   
+ 
+  
+  
+ 
+   
+   
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+    
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+    
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+    
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+    
+ 
+  
