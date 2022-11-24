20ca28e30f3d019c14d683c27783c626c5ae1ec0
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 01:39:31 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1352F | Binary String Reconstruction/79647351.cpp
@@ -0,0 +1,133 @@
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+ 
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define my              unordered_map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define ml(brr,n,type)  type *brr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+ 
+ 
+void anmol()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+ 
+ 
+int32_t main()
+{
+   anmol();
+ 
+int t;
+cin>>t;
+while(t--){
+ 
+ 
+ 
+ 
+int n1,n2,n3;
+cin>>n1>>n2>>n3;
+ 
+string s ="";
+ 
+ 
+if(n2==0){
+ 
+  if(n1){
+   
+   for(int i =0; i<=n1; i++){
+    cout<<0;
+   }
+    cout<<endl; 
+   }
+   else {
+    for(int i =0; i<=n3; i++){
+     cout<<1;
+    }
+ 
+    cout<<endl;
+   }
+ 
+  }
+ 
+  else {
+   
+   for(int i =1; i<=n2+1; i++){
+    if(i%2){
+     s+='1';
+    }
+ 
+    else s +='0';
+   }
+  
+  s.insert(1,string(n1,'0'));
+  s.insert(0,string (n3,'1'));
+ 
+  cout<<s<<endl;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  }
+ 
+ 
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+    
+ 
+  
