9ff7fafa967ac0e7b4f6e73a60c9909b17b44f7f
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 01:28:44 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1316C | Primitive Primes/85804439.cpp
@@ -0,0 +1,283 @@
+ 
+        
+        
+      #include<bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+ 
+#define ff              first
+#define ss              second
+//#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+//#define vi              vector<int>
+#define mii             map<int,int>
+//#define my              unordered_map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define ml(brr,n,type)  type *brr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+ 
+ 
+void anmol()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+const int N = 1e6 + 5 ;
+int a[N] , b[N] , freq[N];
+ 
+ 
+ 
+ 
+int main(){
+ 
+  anmol();
+  
+  int n,m,p;
+ 
+  cin>>n>>m>>p;
+ 
+ 
+  for(int i =0; i<n; i++)
+{
+ cin>>a[i];
+}
+ 
+for(int j =0; j<m; j++){
+ 
+ 
+ cin>>b[j];
+}
+ 
+ 
+int x =0,y =0;
+ 
+ 
+ 
+for(int i =0; i<n; i++){
+ 
+   if(a[i]%p!=0 ){
+    x = i;
+    break;
+   }
+}
+for(int i =0; i<m; i++){
+ 
+   if(b[i]%p!=0 ){
+    y = i;
+    break;
+   }
+ 
+}
+  
+ 
+cout<<x+y<<endl;
+ }
+ 
+ 
+ 
+ 
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+    
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+    
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+    
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+   
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+    
+ 
+  
