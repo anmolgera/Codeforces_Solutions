7e8187ebf6f1fe00c3dac0c3380d94f3f4f77722
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 00:47:46 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1512A | Spy Detected!/112484545.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define int long long
+//typedef pair<int, int> pii;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+const ll mod = 1e9 + 7, N = 2e6 + 7, M = 2e6 + 7, INF = INT_MAX / 10;
+ll powe(ll x, ll y)
+{
+    x = x % mod, y = y % (mod - 1);
+    ll ans = 1;
+    while (y > 0)
+    {
+        if (y & 1)
+        {
+            ans = (1ll * x * ans) % mod;
+        }
+        y >>= 1;
+        x = (1ll * x * x) % mod;
+    }
+    return ans;
+}
+ 
+void anmol()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+ 
+ 
+void solve()
+{
+ 
+int n;
+cin>>n;
+ 
+int a[n];
+map<int,int> mp;
+map<int,int> mp1;
+for(int i =0; i<n; i++){
+    cin>>a[i];
+    mp[a[i]]++;
+    mp1[a[i]] =i;
+}
+int ans =1;
+for(auto i : mp){
+    
+    if(i.second==1){
+     cout<<mp1[i.first]+1<<endl;
+     return;
+    }
+ 
+}
+ 
+return;
+ 
+ 
+ 
+ 
+}
+ 
+int32_t main()
+{
+    int z =1;
+    anmol();
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {   //cout<<"Case #"<<z<<": ";
+        //z++;
+        solve();
+    }
+    return 0;
+}
