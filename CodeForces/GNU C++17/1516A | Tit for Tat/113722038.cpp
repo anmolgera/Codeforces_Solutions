a7667bf529991dc2c8356cf1355e180f63ff41cf
Will you be my Valentine? <49106976+anmolgera@users.noreply.github.com>
2022-11-25 00:44:20 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1516A | Tit for Tat/113722038.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define int long long
+//typedef pair<int, int> pii;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+const ll mod = 1e9 + 7, N = 2e6 + 7, M = 2e6 + 7, INF = INT_MAX / 10;
+ll powe(ll x, ll y)
+{
+    x = x % mod, y = y % (mod - 1);
+    ll ans = 1;
+    while (y > 0)
+    {
+        if (y & 1)
+        {
+            ans = (1ll * x * ans) % mod;
+        }
+        y >>= 1;
+        x = (1ll * x * x) % mod;
+    }
+    return ans;
+}
+ 
+void anmol()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+ 
+ 
+ 
+ 
+int32_t main()
+{
+     anmol();
+     int t;
+     cin>>t;
+     int z =1;
+     //SieveOfEratosthenes();
+     
+     while(t--){
+       //cout<<"Case #"<<z<<": ";
+ 
+       int n;
+       cin>>n;
+ 
+       int k;
+       cin>>k;
+ 
+ 
+ 
+       int a[n];
+ 
+       for(int i=0; i<n; i++){
+           cin>>a[i];
+       }
+       
+    
+       int c =k;
+       for(int i =0; i<n; i++){
+           while(a[i]>0 && c>0){
+               a[i]--;
+               c--;
+           }
+       }
+ 
+       
+       a[n-1]+=(k-c);
+ 
+       for(int i =0; i<n; i++){
+           cout<<a[i]<<" ";
+       }
+       cout<<endl;
+       
+ 
+ 
+ 
+       
+ 
+     }
+}
